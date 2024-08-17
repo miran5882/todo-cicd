9d94d2cfdca382fff7e419609ea0f77b8fea0748
Miran Ahmed <123806039+miran5882@users.noreply.github.com>
2024-08-18 00:36:07 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -19,15 +19,16 @@ usermod -aG docker ubuntu
 usermod -aG docker jenkins
 
 # Install Jenkins
-# Install Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+apt-get update
+sudo apt install openjdk-17-jre-headless
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update
-apt-get install -y openjdk-17-jdk jenkins
-systemctl start jenkins
+sudo apt-get update
+sudo apt-get install jenkins -y
+systemctl start jenkins 
 systemctl enable jenkins
 
 # Install kubectl
