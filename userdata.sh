40715fe9b0e966d545892533a3ab8260b3136c30
Miran Ahmed <123806039+miran5882@users.noreply.github.com>
2024-08-18 00:18:37 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -26,7 +26,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 apt-get update
-apt-get install -y openjdk-11-jdk jenkins
+apt-get install -y openjdk-17-jdk jenkins
 systemctl start jenkins
 systemctl enable jenkins
 
